64120aae3c3bf78efdd21f5f44bfc31ce0431f65
priyanka <prachii8826@gmail.com>
2026-03-30 15:14:15 +0530

    added files

---
+++ backend/backend/src/main/java/com/ecommerce/backend/config/JwtUtil.java
@@ -0,0 +1,36 @@
+package com.ecommerce.backend.config;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.stereotype.Component;
+
+import java.security.Key;
+import java.util.Date;
+
+@Component
+public class JwtUtil {
+    private final String SECRET ="mysecretkeymysecretkeymysecretkey";
+
+    private Key getSignKey() {
+        return Keys.hmacShaKeyFor(SECRET.getBytes());
+    }
+
+    public String generateToken(String username) {
+        return Jwts.builder()
+                .setSubject(username)
+                .setIssuedAt(new Date())
+                .setExpiration(new Date(System.currentTimeMillis() + 1000 * 60* 60))  // 1 Hour
+                .signWith(getSignKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
+
+    public String exactUsername(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSignKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody()
+                .getSubject();
+    }
+}
--- backend/backend/src/main/java/com/ecommerce/backend/controller/AuthController.java
+++ backend/backend/src/main/java/com/ecommerce/backend/controller/AuthController.java
@@ -1,12 +1,12 @@
 package com.ecommerce.backend.controller;
 
+import com.ecommerce.backend.config.JwtUtil;
+import com.ecommerce.backend.dto.LoginRequest;
 import com.ecommerce.backend.model.User;
+import com.ecommerce.backend.repository.UserRepository;
 import com.ecommerce.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.web.bind.annotation.PostMapping;
-//import org.springframework.web.bind.annotation.RequestBody;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//import org.springframework.web.bind.annotation.RestController;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.*;
 
 
@@ -17,8 +17,30 @@ public class AuthController {
     @Autowired
     private UserService service;
 
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
     @PostMapping("/register")
     public User register(@RequestBody User user){
         return service.register(user);
+    }
+
+    @PostMapping("/login")
+    public String login(@RequestBody LoginRequest request) {
+
+        User user = userRepository.findByUsername(request.getUsername())
+                .orElseThrow(() -> new RuntimeException("User not found"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new RuntimeException("Invalid password");
+        }
+
+        return jwtUtil.generateToken(user.getUsername());
     }
 }
